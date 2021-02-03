cbb09fe5280099be65397dddbfdc278186c514e3
ajay-8192 <43032005+ajay-8192@users.noreply.github.com>
2021-02-03 16:04:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let movieName = prompt("Give Movie Name: ")
 
 let h2 = document.createElement('h2')
 
-url = `http://www.omdbapi.com/?t=${movieName}&apikey=9ef3981c`
+url = `https://www.omdbapi.com/?t=${movieName}&apikey=9ef3981c`
 
 fetch(url)
 .then(response => response.json())
